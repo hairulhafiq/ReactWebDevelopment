7c790190609d158547e7837a2274bf04c4889443
hairulhafiq <hairulhafiq@gmail.com>
2020-10-29 18:44:03 +0800

    adding file

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './bowl.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn about js hairul
+        </a> <br></br>
+        <a>
+        welcome to atom
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
